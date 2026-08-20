e3e8121cc8fad12d56c5a3c252e76a46fded8468
GiRyoung <skarlfyd88@gmail.com>
2026-08-20 18:00:42 +0900

    2026-08-20 수업

---
+++ db/transactions.py
@@ -0,0 +1,91 @@
+import oracledb
+from datetime import datetime
+conn = oracledb.connect(user="python_user",password="54321",dsn="localhost/xe")
+cursor = conn.cursor()
+
+def add_transactions():
+    tx_type = input('구분을 입력하세요 (수입/지출): ').strip()
+    amount = input('금액을 입력하세요: ').strip()
+    memo = input('내역을 입력하세요: ').strip()
+    reg_date = input('날짜를 입력하세요 (YYYY-MM-DD, 엔터 시 오늘): ').strip()
+
+    if not reg_date:
+        reg_date = datetime.now().strftime("%Y-%m-%d")
+
+
+
+    # insert 구문 실행
+    sql = "INSERT INTO TRANSACTIONS(tx_type,amount,memo,reg_date) values(:1,:2,:3,:4)"
+    cursor.execute(sql,(tx_type,amount,memo,reg_date)) 
+    conn.commit()
+    if cursor.rowcount > 0:
+        print("등록되었습니다.\n")
+
+def list_transactions():
+    """reg_date 순 asc"""
+    sql = "select tx_id,tx_type,amount,memo,reg_date from transactions order by reg_date"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    
+        
+    if not rows:
+        print("등록된 가계부 목록은 없습니다.\n")
+        return
+    print("-"*50)
+    for row in rows:
+        print(f"{row[0]}. [{row[1]}] {row[2]}원 - {row[3]} ({row[4]})")
+    print("-"*50)
+    print()
+
+def monthly_summary():
+
+    month = input("조회할 월을 입력하세요 (YYYY-MM): ").strip()
+
+    # 2026-08%
+    sql = """SELECT TX_TYPE, sum(amount)
+FROM TRANSACTIONS 
+WHERE REG_DATE LIKE :1
+GROUP BY TX_TYPE"""
+
+    cursor.execute(sql,(month+'%',))
+    rows = cursor.fetchall()
+                
+    if not rows:
+        print("요청하신 해당 월 가계부 내역은 없습니다.\n")
+        return
+    print("-"*50)
+    for row in rows:
+        print(f"{row[0]} : {row[1]}원")
+    print("-"*50)
+    print()
+    
+
+def menu():
+    # 1. 내역 추가 2. 전체 조회 3. 월별 합계 4. 종료
+    while True:
+        print("=== 가계부 관리 ===")
+        print("1. 내역 추가 2. 전체 조회 3. 월별 합계 4. 종료")
+
+        choice = input("선택 : ")
+
+        if choice == "1":
+            add_transactions()
+        elif choice == "2":
+            list_transactions()
+        elif choice == "3":
+            monthly_summary()
+        elif choice == "4":
+            print("종료합니다.")
+            break
+        else:
+            print("번호를 확인해 주세요")
+
+
+
+if __name__ == "__main__":
+    try:
+        menu()
+    finally:
+        cursor.close()
+        conn.close()
+        
